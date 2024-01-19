c701e96aa6a8d1d44b0a5d988e7e489867a168de
b-sharman <nonoesimposible72@gmail.com>
2024-01-18 21:51:14 -0700

    my super informative commit message

---
+++ init.lua
@@ -0,0 +1,60 @@
+vim.opt.expandtab = true
+vim.opt.softtabstop = -1
+vim.opt.shiftwidth = 2
+
+vim.opt.hlsearch = false
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("everforest").setup({
+        background="hard",
+        ui_contrast = "high",
+        colours_override = function(palette)
+          palette.bg0 = "#1f2529"
+        end
+      })
+    end
+  },
+  "neovim/nvim-lspconfig",
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },  
+      })
+    end
+  },
+})
+
+require("everforest").load()
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
