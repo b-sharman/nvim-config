7f57134276d728249f0a06cff1298374f2eb650e
b-sharman <byron.n.sharman@gmail.com>
2024-04-22 12:46:59 -0600

    change telescope keybind

--- init.lua
+++ init.lua
@@ -45,7 +45,7 @@ require("lazy").setup({
 })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 
 -- highlight yanked text for 200ms using the "Visual" highlight group
 vim.cmd[[
